60ca98954a1d6d947d11646fd7b23f2ad2d448c6
OmariLawrence <38704673+OmariLawrence@users.noreply.github.com>
2018-10-06 19:47:34 -0400

    updated main activity java

--- app/src/main/java/com/example/olawr/widgets/MainActivity.java
+++ app/src/main/java/com/example/olawr/widgets/MainActivity.java
@@ -5,13 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     protected ImageButton IB;
     protected ImageView im;
-
+    protected ToggleButton toggleButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +21,19 @@ public class MainActivity extends AppCompatActivity {
         IB = (ImageButton) findViewById(R.id.imageButton1);
         im = (ImageView) findViewById((R.id.imageView));
         im.setVisibility(View.INVISIBLE);
-
+        toggleButton= findViewById(R.id.toggleButton);
         IB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 im.setVisibility(View.VISIBLE);
             }
         });
+
+    }
+    public void ToggleClicked(View v){
+        boolean on =toggleButton.isChecked();
+        if(on)
+            im.setVisibility(View.VISIBLE);
+        else im.setVisibility(View.INVISIBLE);
     }
 }
