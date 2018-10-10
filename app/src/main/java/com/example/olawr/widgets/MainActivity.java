14dc39499c365abae1dedd712a10c4730f0cb65e
OmariLawrence <originalolawrence@gmail.com>
2018-10-10 10:02:38 -0400

    Added Splash Screen Functionality

--- app/src/main/java/com/example/olawr/widgets/MainActivity.java
+++ app/src/main/java/com/example/olawr/widgets/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.olawr.widgets;
 
+import android.content.Intent;
 import android.media.Image;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
 
+// Using handler with postDelayed called runnable run method
+
+            @Override
+
+            public void run() {
+
+                Intent i = new Intent(MainActivity.this, SelectPizza.class);
+
+                startActivity(i);
+
+                // close this activity
+
+                finish();
+
+            }
+
+        }, 5*1000); // wait for 5 seconds
     }
 }
