f35396e2499b45f2e301ec34945dfcf09adb1c15
aundrae <justinramjattan@outlook.com>
2018-11-13 11:15:49 -0400

    Practical 4 Completed

---
+++ app/src/main/java/com/example/olawr/widgets/IntentScreen.java
@@ -0,0 +1,35 @@
+package com.example.olawr.widgets;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class IntentScreen extends AppCompatActivity {
+    TextView view;
+    double value;
+    DecimalFormat df=new DecimalFormat(".##");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_screen);
+        view=findViewById(R.id.view);
+        value=getIntent().getExtras().getInt("mark");
+        view.setText(view.getText().toString()+"\n\n"+value+"="+df.format(value*0.1)+"%");
+        value=value*0.1;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                intent.putExtra("percentage",df.format(value));
+                startActivity(intent);
+            }
+        },3000);
+    }
+
+}
